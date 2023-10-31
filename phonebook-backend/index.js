8a58ca8d427578ae82efde4543d5d46023fb73f9
Ckola99 <Christopherkola@gmail.com>
2023-10-31 15:00:53 +0200

    Linter

--- phonebook-backend/index.js
+++ phonebook-backend/index.js
@@ -1,53 +1,51 @@
-require("dotenv").config();
+require("dotenv").config()
 
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const Person = require("./models/person");
-
-
+const express = require("express")
+const morgan = require("morgan")
+const cors = require("cors")
+const Person = require("./models/person")
 
 // Create Express applications for each port
-const app = express();
-app.use(express.json());
-app.use(cors());
-app.use(express.static('dist'));
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(express.static("dist"))
 
 
 morgan.token("post-data", (request) => {
   if (request.method === "POST") {
-    return JSON.stringify(request.body);
+    return JSON.stringify(request.body)
   }
-  return "";
-});
+  return ""
+})
 
-app.use(morgan("tiny", { stream: { write: (message) => console.log(message) } }));
+app.use(morgan("tiny", { stream: { write: (message) => console.log(message) } }))
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :post-data")
-);
+)
 
 // Route for /api/persons
 app.get("/api/persons", (request, response) => {
   Person.find({}).then((persons) => {
-    response.json(persons);
+    response.json(persons)
   })
-});
+})
 
 // Route for /info
 app.get("/api/info", async (request, response) => {
   try {
-    const count = await Person.countDocuments({});
-    const currentTime = new Date();
+    const count = await Person.countDocuments({})
+    const currentTime = new Date()
     const infoHTML = `
       <p>Phonebook has info for ${count} people</p>
       <p>${currentTime}</p>
-    `;
-    response.send(infoHTML);
+    `
+    response.send(infoHTML)
   } catch (error) {
-    console.error(error);
-    response.status(500).json({ error: "Error fetching info" });
+    console.error(error)
+    response.status(500).json({ error: "Error fetching info" })
   }
-});
+})
 
 //Route for /persons/5
 app.get("/api/persons/:id", (request, response, next) => {
@@ -58,10 +56,10 @@ app.get("/api/persons/:id", (request, response, next) => {
       response.status(404).end()
     }
   }).catch(error => next(error))
-});
+})
 
 //Route for deleting a single entry
-app.delete('/api/persons/:id', (request, response, next) => {
+app.delete("/api/persons/:id", (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
     .then(result => {
       response.status(204).end()
@@ -70,42 +68,42 @@ app.delete('/api/persons/:id', (request, response, next) => {
 })
 
 app.post("/api/persons", (request, response, next) => {
-  const body = request.body;
+  const body = request.body
 
-  const { name, number } = body;
+  const { name, number } = body
   if (!name || !number) {
-    return response.status(400).json({ error: "Name and number are required" });
+    return response.status(400).json({ error: "Name and number are required" })
   }
 
   const person = new Person({
     name: name,
     number: number,
-  });
+  })
   person.save()
     .then(savedPerson => {
-      console.log("New person added:", savedPerson);
-      response.json(savedPerson);
+      console.log("New person added:", savedPerson)
+      response.json(savedPerson)
     })
     .catch(error => next(error))
-});
+})
 
 
-app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body;
-  const { name, number } = body;
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body
+  const { name, number } = body
 
   Person.findByIdAndUpdate(request.params.id,
     { name, number },
     { new: true, runValidators: true, context: "query" }
-    )
+  )
     .then(updatedNote => {
       response.json(updatedNote)
     })
     .catch(error => next(error))
-});
+})
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: "unknown endpoint" })
 }
 
 // handler of requests with unknown endpoint
@@ -113,21 +111,21 @@ app.use(unknownEndpoint)
 
 // Error handling middleware
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
   if (error.name === "CastError") {
-    return response.status(500).send({ error: "malformatted id" });
-  } else if (error.name === 'ValidationError') {
+    return response.status(500).send({ error: "malformatted id" })
+  } else if (error.name === "ValidationError") {
     return response.status(400).json({ error: error.message })
   }
 
-  next(error);
-};
+  next(error)
+}
 
 app.use(errorHandler)
 
 // Start the Express servers for each port
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  console.log(`Server is running on port ${PORT}`)
+})
--- phonebook-backend/mongo.js
+++ phonebook-backend/mongo.js
@@ -1,85 +1,46 @@
-// const mongoose = require('mongoose')
-
-// if (process.argv.length < 3) {
-// 	console.log('give password as argument')
-// 	process.exit(1)
-// }
-
-// const password = process.argv[2]
-
-// const url =
-// 	`mongodb+srv://christopherkola:${password}@cluster0.ivfgiyv.mongodb.net/noteApp?retryWrites=true&w=majority`
-
-// mongoose.set('strictQuery', false)
-// mongoose.connect(url)
-
-// const noteSchema = new mongoose.Schema({
-// 	content: String,
-// 	important: Boolean,
-// })
-
-// const Note = mongoose.model('Note', noteSchema)
-
-// // const note = new Note({
-// // 	content: 'HTML is Easy',
-// // 	important: true,
-// // })
-
-// Note.find({}).then(result => {
-// 	result.forEach(note => {
-// 		console.log(note)
-// 	})
-// 	mongoose.connection.close()
-// })
-
-// // note.save().then(result => {
-// // 	console.log('note saved!')
-// // 	mongoose.connection.close()
-// // })
-
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
 if (process.argv.length < 3) {
-	console.log('give password as argument')
-	process.exit(1)
+  console.log("give password as argument")
+  process.exit(1)
 }
 
-const password = process.argv[2];
+const password = process.argv[2]
 
 const url =
 	`mongodb+srv://christopherkola:${password}@cluster0.ivfgiyv.mongodb.net/phonebookApp?retryWrites=true&w=majority`
 
-mongoose.set("strictQuery", false);
-mongoose.connect(url);
+mongoose.set("strictQuery", false)
+mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-	id: Number,
-	name: String,
-	number: String,
-});
+  id: Number,
+  name: String,
+  number: String,
+})
 
-const Person =  mongoose.model("Person", personSchema);
+const Person =  mongoose.model("Person", personSchema)
 
 if (process.argv.length === 3) {
-	Person.find({}).then((persons) => {
-		console.log(" phonebook: ")
-		persons.forEach((person) => {
-			console.log(`${person.name} ${person.number}`);
-		})
-		mongoose.connection.close()
-	})
+  Person.find({}).then((persons) => {
+    console.log(" phonebook: ")
+    persons.forEach((person) => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
 } else {
-	const [, , password, name, number] = process.argv
+  const [, , password, name, number] = process.argv
 
-	// Add a new entry to the phonebook
-	const person = new Person({
-		id: Math.floor(Math.random() * 10000),
-		name,
-		number,
-	});
+  // Add a new entry to the phonebook
+  const person = new Person({
+    id: Math.floor(Math.random() * 10000),
+    name,
+    number,
+  })
 
-	person.save().then(() => {
-		console.log(`Added ${name} number ${number} to phonebook`);
-		mongoose.connection.close();
-	});
+  person.save().then(() => {
+    console.log(`Added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
 }
